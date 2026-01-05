ac2922ea518a1ec79321fbb8beb0f4d1941c0ab7
bomsbro <qorwogjs@gmail.com>
2026-01-05 16:18:01 +0900

    fix: font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,9 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 
 import AppProviders from '@/shared/core/providers/app-providers';
 import Layout from '@/shared/ui/components/layout/Layout';
 
 import './globals.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'Minimal Blog',
@@ -27,7 +16,7 @@ const RootLayout = ({
   children: React.ReactNode;
 }>) => (
   <html lang="ko">
-    <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+    <body className="font-sans antialiased">
       <AppProviders>
         <Layout>{children}</Layout>
       </AppProviders>
